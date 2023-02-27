61a73f9e4e045bea5c6c5337d33679d213c8be07
Logan Cole <emtlogancole@gmail.com>
2023-02-27 10:19:36 -0600

    feat: visitor class

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,22 @@
 class Visitor
-  
+  attr_reader :name, :height, :spending_money, :preferences
+  def initialize(name, height, spending_input)
+    @name = name
+    @height = height
+    @spending_input = spending_input
+    @spending_money = spending_input[1..-1].to_i
+    @preferences = []
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(num)
+    if @height >= num
+      return true
+    else
+      return false
+    end
+  end
 end
