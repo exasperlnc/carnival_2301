23194017dee8799c36836b6fda9d383224b20f16
Logan Cole <emtlogancole@gmail.com>
2023-02-27 12:11:02 -0600

    feat: most pop ride

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,3 +1,27 @@
+require_relative 'ride'
 class Carnival
+  attr_reader :name, :duration, :rides
+  def initialize(info)
+    @name = info[:name]
+    @duration = info[:duration]
+    @rides = []
+  end
   
+  def add_ride(ride)
+    @rides << ride
+  end
+
+  def pop_ride
+    most_pop_ride = []
+    total_log = []
+    @rides.each do |ride|
+      total_log << ride.total_riders
+    end
+    @rides.each do |ride| 
+      if ride.total_riders == total_log.max
+        most_pop_ride << ride
+      end
+    end
+    most_pop_ride
+  end
 end
---
+++ spec/carnival_spec.rb
@@ -0,0 +1,60 @@
+require 'rspec'
+require './lib/carnival'
+
+RSpec.describe Carnival do
+  before(:each) do
+    @carnival = Carnival.new({:name => "CarnEvil", :duration => "Eternity"})
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @expensive_ride = Ride.new({ name: 'Big Ferris Wheel', min_height: 36, admission_fee: 100, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor3.add_preference(:gentle)
+    @visitor1.add_preference(:water)
+  end
+
+  it 'exists' do
+    expect(@carnival).to be_a Carnival
+  end
+
+  it 'has attributes' do
+    expect(@carnival.name).to eq("CarnEvil")
+    expect(@carnival.duration).to eq("Eternity")
+  end
+
+  it 'adds rides' do
+    @carnival.add_ride(@ride1)
+    @carnival.add_ride(@ride2)
+
+    expect(@carnival.rides.count).to eq(2)
+  end
+
+  it 'has most pop ride' do
+    @carnival.add_ride(@ride1)
+    @carnival.add_ride(@ride2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor3)
+    @ride2.board_rider(@visitor1)
+    @ride2.board_rider(@visitor2)
+
+
+    expect(@carnival.pop_ride).to eq([@ride1])
+  end
+
+  it 'has two rides that tie for most popular' do
+    @carnival.add_ride(@ride1)
+    @carnival.add_ride(@ride2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor3)
+    @ride2.board_rider(@visitor1)
+    @ride2.board_rider(@visitor3)
+
+    expect(@carnival.pop_ride).to eq([@ride1, @ride2])
+  end
+
+end
